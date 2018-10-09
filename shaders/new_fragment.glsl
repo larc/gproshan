34cfd0e569634552597170eca2d8660a85f1c2cc
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-10-09 09:55:32 -0500

    update shader colormap

--- shaders/new_fragment.glsl
+++ shaders/new_fragment.glsl
@@ -18,7 +18,7 @@ float diffuse( vec3 N, vec3 L )
 
 float specular( vec3 N, vec3 L, vec3 E )
 {
-	const float shininess = 8.;
+	const float shininess = 4.;
 	vec3 R = 2.*dot(L,N)*N - L;
 	return pow( max( 0., dot( R, E )), shininess );
 }
@@ -30,14 +30,37 @@ float fresnel( vec3 N, vec3 E )
 	return pow( sqrt( 1. - NE*NE ), sharpness );
 }
 
-// https://github.com/kbinani/colormap-shaders/blob/master/shaders/glsl/MATLAB_hot.frag
+//https://github.com/kbinani/colormap-shaders/blob/master/shaders/glsl/IDL_CB-PuBu.frag
+float colormap_red(float x)
+{
+	if (x < 0.7520372909206926)
+		return (((9.68615208861418E+02 * x - 1.16097242960380E+03) * x + 1.06173672031378E+02) * x - 1.68616613530379E+02) * x + 2.56073136099945E+02;
+	else 
+		return -1.20830453148990E+01 * x + 1.44337397593436E+01;
+}
+
+float colormap_green(float x)
+{
+	if (x < 0.7485333535031721)
+		return (((-4.58537247030064E+02 * x + 5.67323181593790E+02) * x - 2.56714665792882E+02) * x - 1.14205365680507E+02) * x + 2.47073841488433E+02;
+	else
+		return ((-2.99774273328017E+02 * x + 4.12147041403012E+02) * x - 2.49880079288168E+02) * x + 1.93578601034431E+02;
+}
+
+float colormap_blue(float x)
+{
+	if (x < 0.7628468501376879)
+		return ((-5.44257972228224E+01 * x + 2.70890554876532E+01) * x - 9.12766750739247E+01) * x + 2.52166182860177E+02;
+	else
+		return (((4.55621137729287E+04 * x - 1.59960900638524E+05) * x + 2.09530452721547E+05) * x - 1.21704642900945E+05) * x + 2.66644674068694E+04;
+}
+
 vec3 colormap(float x)
 {
-	float t = 0.9;
-    float b = clamp(8.0 / 3.0 * x, 0.0, t);
-    float g = clamp(8.0 / 3.0 * x - 1.0, 0.0, t);
-    float r = clamp(4.0 * x - 3.0, 0.0, t);
-    return vec3(r, g, b);
+	float r = clamp(colormap_red(x) / 255.0, .0, .9);
+	float g = clamp(colormap_green(x) / 255.0, .0, .9);
+	float b = clamp(colormap_blue(x) / 255.0, .0, .9);
+	return vec3(r, g, b);
 }
 
 void main()
@@ -50,18 +73,18 @@ void main()
 	float b = (1. - (1. - d) * (1. - d));
 	vec3 vcolor = vec3(r, g, b);
 	*/
-	vec3 vcolor = colormap(1. - color);
+	vec3 vcolor = colormap(color);
 
 
 	// lines
 	if(lines)
 	{
 		float h = color;
-		h = h * 30.;
+		h = h * 40.;
 		h = h - floor( h );
 		h = (1. / (1. + exp(-100.*(h - .55)))) + (1. / (1. + exp(-100.*(-h + .45))));
 		h = 1. - h;
-		vcolor.xyz = vec3(h, h, h) + (1. - h) * vcolor.xyz;
+		vcolor.xyz = vec3(0, 0, 0) + (1. - h) * vcolor.xyz;
 	}
 
  	vec3 N;
@@ -83,7 +106,7 @@ void main()
 	vec3 R = 2.*dot(L,N)*N - L;
 	vec3 one = vec3( 1., 1., 1. );
 
-	FragColor.rgb = diffuse(N,L) * vcolor + .5 * specular(N,L,E) * one + .5 * fresnel(N,E) * one;
+	FragColor.rgb = diffuse(N,L) * vcolor + .1 * specular(N,L,E) * one + .5 * fresnel(N,E) * one;
 	FragColor.a = 1.;
 }
 
--- src/test_geodesics_ptp.cpp
+++ src/test_geodesics_ptp.cpp
@@ -65,7 +65,7 @@ void main_test_geodesics_ptp(const int & nargs, const char ** args)
 			Error[4] = Error[3] = INFINITY;
 		#else
 			Time[4] = test_heat_method_cholmod(Error[3], Time[3], exact, mesh, source, n_test);
-			if(n_vertices < 120000)	
+			if(n_vertices < 100000)	
 				Time[6] = test_heat_method_gpu(Error[4], Time[5], exact, mesh, source, n_test);
 			else
 			{
