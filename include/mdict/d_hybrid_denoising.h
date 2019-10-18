36f16c33a936261eefca49f6d320fb9a2e85a106
lizeth <lztfuentespz@gmail.com>
2019-10-18 13:31:54 +0200

    new hybrid test added

---
+++ include/mdict/d_hybrid_denoising.h
@@ -0,0 +1,16 @@
+#ifndef D_HYBRID_DENOISING_H
+#define D_HYBRID_DENOISING_H
+
+#include "mdict.h"
+
+
+// geometry processing and shape analysis framework
+// mesh dictionary learning and sparse coding namespace
+namespace gproshan::mdict {
+
+void test_hybrid_denoising(const string & file);
+
+} // namespace gproshan::mdict
+
+#endif // D_HYBRID_DENOISING_H
+
---
+++ test_hybrid_denoising.cpp
@@ -0,0 +1,9 @@
+#include "mdict/d_hybrid_denoising.h"
+
+int main(int nargs, const char ** args)
+{
+	gproshan::mdict::test_hybrid_denoising(args[1]);
+
+	return 0;
+}
+
