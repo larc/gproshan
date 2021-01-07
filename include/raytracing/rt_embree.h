bbfdb6af4cfc6fed348addf6e2a740970803c835
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2021-01-07 20:49:18 +0100

    rt: organizing embree code

--- include/raytracing/rt_embree.h
+++ include/raytracing/rt_embree.h
@@ -17,91 +17,49 @@ namespace gproshan::rt {
 
 class embree : public raytracing
 {
-	struct ray_hit: public RTCRayHit
-	{
-		ray_hit(const glm::vec3 & origin = glm::vec3(0.0f),
-				const glm::vec3 & direction = glm::vec3(0.0f),
-				float near = 1e-5f,
-				float far = FLT_MAX)
+	protected:
+		struct ray_hit : public RTCRayHit
 		{
-			ray.org_x = origin.x;
-			ray.org_y = origin.y;
-			ray.org_z = origin.z;
-			ray.tnear = near;
+			ray_hit(const glm::vec3 & p_org = glm::vec3(0.0f),
+					const glm::vec3 & v_dir = glm::vec3(0.0f),
+					float near = 1e-5f,
+					float far = FLT_MAX);
 
-			ray.dir_x = direction.x;
-			ray.dir_y = direction.y;
-			ray.dir_z = direction.z;
-			ray.time = 0.0f;
+			glm::vec3 org() const;
+			glm::vec3 dir() const;
+			glm::vec3 color(const rt_mesh & mesh) const;
+			glm::vec3 normal(const rt_mesh & mesh, const bool & flat = false) const;
+			glm::vec3 position() const;
+		};
 
-			ray.tfar = far;
-			ray.mask = 0;
-			ray.flags = 0;
 
-			//hit.primID = RTC_INVALID_GEOMETRY_ID;
-			hit.geomID = RTC_INVALID_GEOMETRY_ID;
-			hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
-		}
-
-		const glm::vec3 org() const
-		{
-			return {ray.org_x, ray.org_y, ray.org_z};
-		}
+		RTCDevice device;
+		RTCScene scene;	
+		RTCIntersectContext intersect_context;
 		
-		const glm::vec3 dir() const
-		{
-			return {ray.dir_x, ray.dir_y, ray.dir_z};
-		}
-		
-		const glm::vec3 color(const rt_mesh & mesh)
-		{
-			const vertex & c = mesh.pointcloud ?	mesh->color(hit.primID) :
-													mesh->shading_color(hit.primID, 1.0 - hit.u - hit.v, hit.u, hit.v);	
-			return glm::vec3(c.x, c.y, c.z);
-		}
-
-		const glm::vec3 normal(const rt_mesh & mesh, const bool & flat = false) const
-		{
-			if(flat || mesh.pointcloud)
-				return glm::normalize(glm::vec3(hit.Ng_x, hit.Ng_y, hit.Ng_z));
-			
-			const vertex & n = mesh->shading_normal(hit.primID, 1.0 - hit.u - hit.v, hit.u, hit.v);	
-			return glm::normalize(glm::vec3(n.x, n.y, n.z));
-		}
-
-		const glm::vec3 position() const
-		{
-			return org() + ray.tfar * dir();
-		}
-	};
-
-
-	RTCDevice device;
-	RTCScene scene;	
-	RTCIntersectContext intersect_context;
-	
 	public:
 		static float pc_radius;
 
 	public:
 		embree(const std::vector<che *> & meshes, const bool & pointcloud = false);
-		~embree();
+		virtual ~embree();
 	
-	private:
+	protected:
 		bool intersect(ray_hit & r);
 		bool occluded(ray_hit & r);
 
-		glm::vec4 intersect_li(const glm::vec3 & org, const glm::vec3 & dir, const glm::vec3 & light, const bool & flat);
-		float intersect_depth(const glm::vec3 & org, const glm::vec3 & dir);
-		
 		void build_bvh(const std::vector<che *> & meshes, const bool & pointcloud = false);
 		index_t add_sphere(const glm::vec4 & xyzr);
 		index_t add_mesh(const che * mesh);
 		index_t add_point_cloud(const che * mesh);
 
 		float pointcloud_hit(glm::vec3 & position, glm::vec3 & normal, glm::vec3 & color, ray_hit r);
+
 		glm::vec4 li(const glm::vec3 & light, const glm::vec3 & position, const glm::vec3 & normal, const glm::vec3 & color, const float & near = 1e-5f);
 		glm::vec4 li(ray_hit r, const glm::vec3 & light, const bool & flat);
+		
+		glm::vec4 intersect_li(const glm::vec3 & org, const glm::vec3 & dir, const glm::vec3 & light, const bool & flat);
+		float intersect_depth(const glm::vec3 & org, const glm::vec3 & dir);
 };
 
 
