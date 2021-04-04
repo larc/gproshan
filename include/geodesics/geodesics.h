f4eb0c5b08d3c9aefa461fab3a93e16f44305f4e
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2021-04-04 15:10:24 +0200

    cmake add -Wall and -Wextra compile options

--- include/geodesics/geodesics.h
+++ include/geodesics/geodesics.h
@@ -71,11 +71,11 @@ class geodesics
 	private:
 		void execute(che * mesh, const std::vector<index_t> & sources, const params & p);
 		void run_fastmarching(che * mesh, const std::vector<index_t> & sources, const size_t & n_iter, const real_t & radio, const fm_function_t & fun);
-		void run_parallel_toplesets_propagation_cpu(che * mesh, const std::vector<index_t> & sources, const size_t & n_iter, const real_t & radio);
+		void run_parallel_toplesets_propagation_cpu(che * mesh, const std::vector<index_t> & sources);
 		void run_heat_method(che * mesh, const std::vector<index_t> & sources);
 
 #ifdef GPROSHAN_CUDA
-		void run_parallel_toplesets_propagation_gpu(che * mesh, const std::vector<index_t> & sources, const size_t & n_iter, const real_t & radio);
+		void run_parallel_toplesets_propagation_gpu(che * mesh, const std::vector<index_t> & sources);
 		void run_heat_method_gpu(che * mesh, const std::vector<index_t> & sources);
 #endif // GPROSHAN_CUDA
 
