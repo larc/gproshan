58dc07cba1b02af52f26951f7d14ee51246b7152
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-10-21 11:12:37 +0200

    rt_optix: add eval_li function

--- include/gproshan/raytracing/rt_utils.h
+++ include/gproshan/raytracing/rt_utils.h
@@ -39,7 +39,8 @@ struct random
 };
 
 
-template <class T>
+
+template <class T, class Occluded>
 struct eval_hit
 {
 	float u, v;
@@ -66,15 +67,27 @@ struct eval_hit
 		color = ((1.f - u - v) * ca + u * cb + v * cc) / 255;
 		normal = (1.f - u - v) * mesh.VN[a] + u * mesh.VN[b] + v * mesh.VN[c];
 	}
+
+	__host__ __device__
+	vec<T, 3> eval_li(const vec<T, 3> * lights, const int & n_lights, Occluded occluded)
+	{
+		vec<T, 3> li, wi;
+		float light_dist, dot_wi_normal;
+
+		for(int i = 0; i < n_lights; ++i)
+		{
+			wi = lights[i] - position;
+			light_dist = length(wi);
+			wi /= light_dist;
+			dot_wi_normal = dot(wi, normal);
+
+			li += (dot_wi_normal < 0 ? -dot_wi_normal : dot_wi_normal) * (occluded(position, wi, light_dist) ? 0.4f : 1.0f) * color;
+		}
+
+		return li;
+	}
 };
 
-
-template <class Fun>
-__host__ __device__
-bool eval_occluded(Fun f)
-{
-	return f();
-}
 
 template <class T>
 __host__ __device__
--- src/gproshan/raytracing/rt_optix.cu
+++ src/gproshan/raytracing/rt_optix.cu
@@ -43,12 +43,34 @@ extern "C" __global__ void __closesthit__radiance()
 	
 	const int primID = optixGetPrimitiveIndex();
 	float2 bar = optixGetTriangleBarycentrics();
-	eval_hit<float> hit(mesh, primID, bar.x, bar.y);
 
 	OptixTraversableHandle gas = optixGetGASTraversableHandle();
 	const unsigned int sbtID = optixGetSbtGASIndex();
 	const float time = optixGetRayTime();
 
+	auto occluded = [&](const vec3 & position, const vec3 & wi, const float & light_dist) -> bool
+					{
+						unsigned int occluded = 1;
+						optixTrace( optix_params.traversable,
+									* (float3 *) &position,
+									* (float3 *) &wi,
+									1e-3f,					// tmin
+									light_dist - 1e-3f,		// tmax
+									0.0f,					// rayTime
+									OptixVisibilityMask(255),
+									OPTIX_RAY_FLAG_DISABLE_ANYHIT
+									| OPTIX_RAY_FLAG_DISABLE_CLOSESTHIT
+									| OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT,
+									1,	// SBT offset
+									2,	// SBT stride
+									1,	// missSBTIndex
+									occluded);
+
+						return occluded != 0;
+					};
+
+	eval_hit<float, decltype(occluded)> hit(mesh, primID, bar.x, bar.y);
+	
 	vertex data[3];
 	optixGetTriangleVertexData(gas, primID, sbtID, time, (float3 *) data);
 
@@ -56,42 +78,10 @@ extern "C" __global__ void __closesthit__radiance()
 	const vertex & B = data[1];
 	const vertex & C = data[2];
 
-	const vertex normal = optix_params.flat ? normalize((B - A) * (C - A)) : hit.normal; 
+	hit.normal = optix_params.flat ? normalize((B - A) * (C - A)) : hit.normal; 
+	hit.position = (1.f - hit.u - hit.v) * A + hit.u * B + hit.v * C;
 
-
-	const vertex * lights = optix_params.lights;
-	const vertex position = (1.f - hit.u - hit.v) * A + hit.u * B + hit.v * C;
-
-	vertex li;
-	for(int i = 0; i < optix_params.n_lights; ++i)
-	{
-		vertex wi = lights[i] - position;
-		float light_dist = length(wi);
-		wi /= light_dist;
-		float dot_wi_normal = (wi, normal);
-
-		bool occluded = eval_occluded([&]()
-		{ 
-			unsigned int occluded = 1;
-			optixTrace( optix_params.traversable,
-						* (float3 *) &position,
-						* (float3 *) &wi,
-						1e-3f,					// tmin
-						light_dist - 1e-3f,		// tmax
-						0.0f,					// rayTime
-						OptixVisibilityMask(255),
-						OPTIX_RAY_FLAG_DISABLE_ANYHIT
-						| OPTIX_RAY_FLAG_DISABLE_CLOSESTHIT
-						| OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT,
-						1,	// SBT offset
-						2,	// SBT stride
-						1,	// missSBTIndex
-						occluded);
-			return occluded != 0;
-		});
-
-		li += (dot_wi_normal < 0 ? -dot_wi_normal : dot_wi_normal) * (occluded ? 0.4f : 1.0f) * hit.color;
-	}
+	vertex li = hit.eval_li(optix_params.lights, optix_params.n_lights, occluded);
 
 	vertex & pixel_color = *ray_data<vertex>();
 	pixel_color = (pixel_color * optix_params.n_samples + li / optix_params.n_lights) / (optix_params.n_samples + 1);
