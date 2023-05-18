27994d9b2ed0cffc29c17def45968a8281cd43e4
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2023-05-18 15:17:39 +0200

    update .gitignore

--- include/gproshan/raytracing/splat_embree.h
+++ include/gproshan/raytracing/splat_embree.h
@@ -16,6 +16,28 @@ class splat_embree: public splat, public embree
 		{
 			build_bvh(pointclouds, {mat4::identity()});
 		}
+
+		vec3 closesthit_radiance(const vertex & org, const vertex & dir, const vertex * lights, const int & n_lights, const vertex & cam_pos, const bool & flat)
+		{
+			ray_hit r(org, dir);
+			if(!intersect(r)) return {};
+
+			const CHE * mesh = g_meshes[r.hit.geomID];
+
+			eval_hit hit;
+			/*
+			if(mesh->n_trigs)
+				hit = {*mesh, r.hit.primID, r.hit.u, r.hit.v, sc};
+			hit.position = r.pos();
+			hit.normal = flat ? r.normal() : hit.normal;
+			*/
+			return eval_li(	hit, lights, n_lights, cam_pos,
+							[&](const vec3 & position, const vec3 & wi, const float & light_dist) -> bool
+							{
+								ray_hit ro(position, wi, 1e-3f, light_dist - 1e-3f);
+								return occluded(ro);
+							});
+		}
 };
 
 
