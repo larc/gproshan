00f55ae5f60ef096fab8a15cbfd38ad405da6a88
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2023-05-23 16:37:28 +0200

    splat: removed old version

--- src/gproshan/viewer/viewer.cpp
+++ src/gproshan/viewer/viewer.cpp
@@ -17,7 +17,6 @@
 #include <gproshan/viewer/scene_viewer.h>
 
 #include <gproshan/raytracing/embree.h>
-#include <gproshan/raytracing/rt_embree_splat_ch.h>
 #include <gproshan/raytracing/splat.h>
 #include <gproshan/raytracing/splat_embree.h>
 #include <gproshan/raytracing/splat_optix.h>
@@ -769,24 +768,14 @@ bool viewer::m_setup_raytracing(viewer * view)
 	static double time = 0;
 	static float pc_radius = 0.01;
 
-	ImGui::Combo("rt", &rt, "Select\0Embree\0OptiX\0Splat Test\0splat embree\0splat optix\0\0");
+	ImGui::Combo("rt", &rt, "Select\0Embree\0OptiX\0Splat Test\0Splat Embree\0Splat OptiX\0\0");
 	ImGui::InputFloat("pc_radius (if render_pointcloud)", &pc_radius, 0, 0, "%.3f");
 
-	static int rt_opt = 0;
-	static float angle = acos(rt::embree_splat_ch::n_threshold) * 180 / M_PI;
-	static const size_t min_neighbors = 1 << 3;
-	static const size_t max_neighbors = 1 << 10;
+	//static const size_t min_neighbors = 1 << 3;
+	//static const size_t max_neighbors = 1 << 10;
 
-	ImGui::SliderInt("k_neighbors", &rt::splat::k, 1, 1 << 8);
-	ImGui::Combo("splat_opt", &rt_opt, "Mesh\0Splat\0\0");
-	if(rt_opt)
-	{
-		ImGui::Checkbox("show_chsplats", &rt::embree_splat_ch::show_chsplats);
-		// ImGui::SliderFloat("r_threshold", &rt::embree_splat_ch::r_threshold, 0.01, 1, "%.2f");
-		ImGui::SliderFloat("n_threshold", &angle, 0, 90, "%.2f");
-		ImGui::SliderScalar("max_neighbors", ImGuiDataType_U64, &rt::embree_splat_ch::max_neighbors, &min_neighbors, &max_neighbors, "%lu");
-		ImGui::SliderInt("k_neighbors", &rt::embree_splat_ch::k_neighbors, 1 << 2, 1 << 8);
-	}
+	if(rt > 2)
+		ImGui::SliderInt("k_neighbors", &rt::splat::k, 1, 1 << 8);
 
 	if(ImGui::Button("Build"))
 	{
@@ -797,13 +786,7 @@ bool viewer::m_setup_raytracing(viewer * view)
 			case R_EMBREE:
 				delete mesh.rt_embree;
 				TIC(time);
-				if(!rt_opt)
 					mesh.rt_embree = new rt::embree({mesh}, {mesh.model_mat}, mesh.render_pointcloud, pc_radius);
-				else
-				{
-					rt::embree_splat_ch::n_threshold = cos(angle * M_PI / 180);
-					mesh.rt_embree = new rt::embree_splat_ch({view->active_mesh()}, {mesh.model_mat});
-				}
 				TOC(time);
 				snprintf(view->status_message, sizeof(view->status_message), "build embree in %.3fs", time);
 				break;
@@ -821,9 +804,9 @@ bool viewer::m_setup_raytracing(viewer * view)
 			case 3:
 			{
 				TIC(time);
-				rt::splat splat_test({mesh}, {mesh.model_mat});
-				mesh.update_vbo_heatmap();
+					rt::splat splat_test({mesh}, {mesh.model_mat});
 				TOC(time);
+				mesh.update_vbo_heatmap();
 				sprintf(view->status_message, "build splats in %.3fs", time);
 				for(che * pc: splat_test.pointclouds)
 					view->add_mesh(new che(*pc), false);
@@ -845,6 +828,7 @@ bool viewer::m_setup_raytracing(viewer * view)
 				TIC(time);
 					mesh.rt_optix = new rt::splat_optix({mesh}, {mesh.model_mat});
 				TOC(time);
+				mesh.update_vbo_heatmap();
 				sprintf(view->status_message, "build splat optix in %.3fs", time);
 			#endif // GPROSHAN_OPTIX
 				break;
