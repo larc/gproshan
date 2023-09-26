2cb34ed3f6f91b010bad5a3cda4c545a74bed2f9
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2023-09-26 16:15:15 +0200

    splat: shading gaussian fixed parameters

--- include/gproshan/raytracing/splat_utils.h
+++ include/gproshan/raytracing/splat_utils.h
@@ -112,9 +112,10 @@ void splat_hit(t_eval_hit<T> & hit, const CHE & pc, const splats_data & sd, cons
 
 	index_t begin = s.begin;
 	index_t end = s.end;
+	T w = length(pc.GT[s.end - 1] - pc.GT[s.begin]);
 
 	const index_t & h = binary_search(sd.morton_codes, begin, end - 1, s.morton2d(x));
-	const real_t & sigma2 = 0.1;
+	const real_t & std = 0.000001;
 
 	vec<T, 3> & color = hit.Kd = {0, 0, 0};
 	vec<T, 3> & normal = hit.normal = {0, 0, 0};
@@ -125,15 +126,14 @@ void splat_hit(t_eval_hit<T> & hit, const CHE & pc, const splats_data & sd, cons
 	begin = begin < s.begin || begin > end ? s.begin : begin;
 	end = end > s.end ? s.end : end;
 
-	T w, sum_w = 1e-5;
-	for(index_t v = s.begin; v < s.end; ++v)
+	T sum_w = 1e-5;
+	for(index_t v = begin; v < end; ++v)
 	{
-		vec<T, 3> p = pc.GT[v];
-		p.z() = p.x() * p.x() + p.y() * p.y();
+		const vec<T, 3> & p = pc.GT[v];
 		const vec<T, 3> & q = dot(d, p - x) * d + x;
 
-		w = 1 - length(p - q);
-		if(w < 0) w = 0;
+		w = length(p - q);
+		w = exp(- w * w / std); 
 		sum_w += w;
 
 		const che::rgb_t & c = pc.VC[v];
