7768982f1e22dd272bdb42ff8b37e9eb6d3fd759
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2023-10-03 13:50:46 +0200

    splat: #8 - printing build times

--- apps/test_splats.cpp
+++ apps/test_splats.cpp
@@ -45,7 +45,7 @@ int main()
 	while(fscanf(fp, "%s", str) != EOF)
 	{
 		fscanf(fp, "%p %s %lu %*d %u", &r.view, str, &r.mnv, &rt);
-		if(rt <= 3)
+		if(rt < 4)
 		{
 			fscanf(fp, "%*f");
 			continue;
@@ -53,26 +53,28 @@ int main()
 
 		fscanf(fp, "%p %lu %lu %lu", &r.splat, &r.pnv, &r.pnt, &r.ns);
 		fscanf(fp, "%lf %lf %lf %lf %lf", &r.tknn, &r.tseg, &r.tvor, &r.tisp, &r.time);
-		
+	
 		if(set_pcs.find(str) != set_pcs.end())
 			table[rt - 4][str] = r;
 	}
 
 	fclose(fp);
-	
-	
+
+
 	fp = fopen("results/table_splats.tex", "w");
 	for(const auto & s: pcs)
 	{
 		const row & e = table[0][s];
 		const row & o = table[1][s];
 
-		fprintf(fp, "%20s & embree & %16lu & %16lu & %16lu & %16lu \\\\\n", s.c_str(), e.mnv, e.pnv, e.pnt, e.ns);
-		fprintf(fp, "%20s &  optix & %16lu & %16lu & %16lu & %16lu \\\\\\hline\n", s.c_str(), o.mnv, o.pnv, o.pnt, o.ns);
-		
+		fprintf(fp, "%20s & embree & %16lu & %16lu & %16lu & %16lu \\\\        %% %12f %12f %12f %12f %12f\n",
+					s.c_str(), e.mnv, e.pnv, e.pnt, e.ns, e.tknn, e.tseg, e.tvor, e.tisp, e.time);
+		fprintf(fp, "%20s &  optix & %16lu & %16lu & %16lu & %16lu \\\\\\hline  %% %12f %12f %12f %12f %12f\n", 
+					s.c_str(), o.mnv, o.pnv, o.pnt, o.ns, o.tknn, o.tseg, o.tvor, o.tisp, o.time);
+	
 		printf("cp %s_%p results/frametime_%s_embree\n", tmp_file_path("frametime").c_str(), e.view, s.c_str());
 		printf("cp %s_%p results/frametime_%s_optix\n", tmp_file_path("frametime").c_str(), o.view, s.c_str());
-		
+	
 		printf("cp %s_%p results/histogram_%s_embree\n", tmp_file_path("histogram").c_str(), e.splat, s.c_str());
 		printf("cp %s_%p results/histogram_%s_optix\n", tmp_file_path("histogram").c_str(), o.splat, s.c_str());
 	}
