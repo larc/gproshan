3e63deb7d41f1a5b42dee2a1baa59343aed9b432
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2023-10-26 16:19:00 +0200

    util: adding partitions iterator

--- include/gproshan/util.h
+++ include/gproshan/util.h
@@ -3,9 +3,46 @@
 
 #include <gproshan/include.h>
 
+#include <cassert>
+#include <vector>
+
 
 // geometry processing and shape analysis framework
 namespace gproshan {
+
+
+class partitions
+{
+	struct part;
+
+	std::vector<index_t> splits;
+	index_t * sorted = nullptr;
+
+	public:
+		part operator () (const index_t & i) const;
+};
+
+struct partitions::part
+{
+	struct iterator;
+
+	index_t _begin = 0;
+	index_t _end = 0;
+	const index_t * sorted = nullptr;
+
+	iterator begin() const;
+	iterator end() const;
+};
+
+struct partitions::part::iterator
+{
+	index_t i = 0;
+	const index_t * sorted = nullptr;
+
+	iterator & operator ++ ();
+	bool operator != (const iterator & it) const;
+	const index_t & operator * ();
+};
 
 
 void copy_real_t_array(float * destination, const float * source, const size_t & n_elem);
--- src/gproshan/util.cpp
+++ src/gproshan/util.cpp
@@ -7,6 +7,39 @@
 namespace gproshan {
 
 
+partitions::part partitions::operator () (const index_t & i) const
+{
+	assert(i > 0 && i < splits.size());
+	return {splits[i - 1], splits[i], sorted};
+}
+
+partitions::part::iterator partitions::part::begin() const
+{
+	return {_begin, sorted};
+}
+
+partitions::part::iterator partitions::part::end() const
+{
+	return {_end, sorted};
+}
+
+partitions::part::iterator & partitions::part::iterator::operator ++ ()
+{
+	++i;
+	return *this;
+}
+
+bool partitions::part::iterator::operator != (const iterator & it) const
+{
+	return i != it.i;
+}
+
+const index_t & partitions::part::iterator::operator * ()
+{
+	return sorted ? sorted[i] : i;
+}
+
+
 void copy_real_t_array(float * destination, const float * source, const size_t & n_elem)
 {
 	memcpy(destination, source, n_elem * sizeof(float));
